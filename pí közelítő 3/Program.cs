d784f6fb0fb7a9cfffa5d361ed79f9cda2366d28
Kukac <thekukac9@gmail.com>
2018-05-01 01:02:07 +0200

    Initial commit

---
+++ pí közelítő 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///double pi = 4 * (4 * Math.Atan2(1, 5) - Math.Atan2(1, 239)); // !!Machin-formula!! // gyors, sok tag
+
+
+            double pi2 = Math.Pow(1, 1);// / 1; //Euler féle módszer // ugyszint gyors
+            for (int i = 3; ; i += 2)
+            {
+                pi2 -=  Math.Pow(pi2, i) / i;
+                pi2 = Math.Atan(pi2);
+                i += 2;
+                pi2 += Math.Pow(pi2, i) / i;
+                pi2 = 4 * Math.Atan(pi2);
+                Console.WriteLine(Math.Abs(pi2/2));
+                Console.ReadKey();
+            }
+
+            //Console.WriteLine(pi);
+            
+        }
+    }
+}
